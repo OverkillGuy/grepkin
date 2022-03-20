d1db51d2778c36b18f78c63ad9f52254377116f0
Jb DOYON <jb@jiby.tech>
2022-03-20 02:51:15 +0000

    Add boolean comparison of feature and regexed test

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,38 @@ use std::fs;
 use gherkin;
 use regex::Regex;
 
+/// Computes equality of gherkin::Feature objects
+/// in terms of Gherkin content
+/// but ignoring the Spans (line/col number) where text was found
+fn approximately_eq(reference: &gherkin::Feature, other: &gherkin::Feature) -> bool {
+    let is_feature_equal: bool = reference.keyword == other.keyword
+        && reference.name == other.name
+        && reference.description == other.description
+        && reference.background == other.background
+        && reference.description == other.description;
+    // Assuming scenarios come out in same order = zip-able
+    let are_scenarios_equal: bool =
+        reference.scenarios.iter()
+                           .zip(other.scenarios.iter())
+                           .map(|s| s.0.keyword == s.1.keyword
+                                && s.0.name == s.1.name
+                                // Dive into Vec<steps> of Scenario the same way
+                                && s.0.steps.iter().zip(s.1.steps.iter())
+                                    .map(|t| t.0.keyword == t.1.keyword
+                                         && t.0.ty == t.1.ty
+                                         && t.0.value == t.1.value)
+                                    .reduce(|acc, mk | acc && mk).unwrap())  // reduce by AND: Vec<steps>
+                           .reduce(|acc, mk | acc && mk)  // reduce by AND: Vec<scenarios>
+                           .unwrap();
+    is_feature_equal && are_scenarios_equal
+}
+
 
 fn main() {
     let feature_file = Path::new("features/checking_guess_valid_word.feature");
     let env = gherkin::GherkinEnv::new("en").ok().expect("Failed to get Gherkin environment");
-    let parsed = gherkin::Feature::parse_path(feature_file, env);
-    println!("Feature says:\n{:?}", parsed);
+    let feature_parsed = gherkin::Feature::parse_path(feature_file, env).unwrap();
+    println!("Feature says:\n{:?}", feature_parsed);
 
     let gherkin_keywords = "Given|When|Then|And|But|Scenario|Background|Feature|In order to|As a|I want to|I need to|So that";
     let gherkin_regex_str = format!(r".*({})(.*)", gherkin_keywords);
@@ -25,7 +51,10 @@ fn main() {
     // println!("Test matches:\n {}", gherkin_test_matches);
 
     let test_env = gherkin::GherkinEnv::new("en").ok().expect("Failed to get Gherkin environment");
-    let test_parsed = gherkin::Feature::parse(gherkin_test_matches, test_env);
+    // Assumes a single Feature per test file, assumes feature is present, no raw Scenarios
+    let test_parsed = gherkin::Feature::parse(gherkin_test_matches, test_env).unwrap();
     println!("Test says:\n{:?}", test_parsed);
-    // TODO: Match the gherkin::Feature's Vec<Scenarios> to the regex captures, giving percent match
+    println!("Naive equality: {}\nCustom, approximate, match: {}",
+             test_parsed == feature_parsed,
+             approximately_eq(&test_parsed, &feature_parsed));
 }
