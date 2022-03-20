23c1f8d1e7757f5685bf3c1d661dfca69bd21209
Jb DOYON <jb@jiby.tech>
2022-03-20 03:14:38 +0000

    Add gherkin extracting function

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,20 @@ fn approximately_eq(reference: &gherkin::Feature, other: &gherkin::Feature) -> b
     is_feature_equal && are_scenarios_equal
 }
 
+/// Extract lines of text containing Gherkin keywords
+///
+/// Returned as a String with one Gherkin statement per line
+fn extract_gherkin_text(text: &str) -> String {
+    let gherkin_keywords = "Given|When|Then|And|But|Scenario|Background|Feature|In order to|As a|I want to|I need to|So that";
+    let gherkin_regex_str = format!(r".*({})(.*)", gherkin_keywords);
+    let gherkin_comments_regex = Regex::new(gherkin_regex_str.as_str()).unwrap();
+    gherkin_comments_regex
+        .captures_iter(text)
+        .map(|cap| format!("{}{}\n", &cap[1], // newline-separated keyword+text concat
+                           // Remove Python docstring delimiters at the end of line
+                           &cap[2].trim_end_matches("\"\"\"")))
+        .collect()
+}
 
 fn main() {
     let feature_file = Path::new("features/checking_guess_valid_word.feature");
@@ -37,18 +51,10 @@ fn main() {
     let feature_parsed = gherkin::Feature::parse_path(feature_file, env).unwrap();
     println!("Feature says:\n{:?}", feature_parsed);
 
-    let gherkin_keywords = "Given|When|Then|And|But|Scenario|Background|Feature|In order to|As a|I want to|I need to|So that";
-    let gherkin_regex_str = format!(r".*({})(.*)", gherkin_keywords);
-    let gherkin_comments_regex = Regex::new(gherkin_regex_str.as_str()).unwrap();
-
     let test_file = Path::new("tests/test_checking_guess_valid_word.py");
     let test_text = fs::read_to_string(test_file).expect("Failed to read test file");
-    let gherkin_test_matches: String =
-        gherkin_comments_regex.captures_iter(&test_text)
-                              .map(|cap| format!("{}{}\n", &cap[1],
-                                                 &cap[2].trim_end_matches(r#"""""#))) // Python docstring delimiters
-                              .collect();
-    // println!("Test matches:\n {}", gherkin_test_matches);
+    let gherkin_test_matches = extract_gherkin_text(&test_text);
+    println!("Test matches:\n{}", gherkin_test_matches);
 
     let test_env = gherkin::GherkinEnv::new("en").ok().expect("Failed to get Gherkin environment");
     // Assumes a single Feature per test file, assumes feature is present, no raw Scenarios
