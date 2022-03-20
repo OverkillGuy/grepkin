a699f24cb26484fde8a4c60816d1ffd9642f9ade
Jb DOYON <jb@jiby.tech>
2022-03-20 02:14:34 +0000

    Add parsing of test file as Gherkin struct

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let feature_file = Path::new("features/checking_guess_valid_word.feature");
     let env = gherkin::GherkinEnv::new("en").ok().expect("Failed to get Gherkin environment");
     let parsed = gherkin::Feature::parse_path(feature_file, env);
-    println!("Hello, world!\n{:?}", parsed);
+    println!("Feature says:\n{:?}", parsed);
 
     let gherkin_keywords = "Given|When|Then|And|But|Scenario|Background|Feature|In order to|As a|I want to|I need to|So that";
     let gherkin_regex_str = format!(r".*({})(.*)", gherkin_keywords);
@@ -17,7 +17,15 @@ fn main() {
 
     let test_file = Path::new("tests/test_checking_guess_valid_word.py");
     let test_text = fs::read_to_string(test_file).expect("Failed to read test file");
-    for capture in gherkin_comments_regex.captures_iter(&test_text) {
-        println!("{} = '{}'", &capture[1], &capture[2]);
-    }
+    let gherkin_test_matches: String =
+        gherkin_comments_regex.captures_iter(&test_text)
+                              .map(|cap| format!("{}{}\n", &cap[1],
+                                                 &cap[2].trim_end_matches(r#"""""#))) // Python docstring delimiters
+                              .collect();
+    // println!("Test matches:\n {}", gherkin_test_matches);
+
+    let test_env = gherkin::GherkinEnv::new("en").ok().expect("Failed to get Gherkin environment");
+    let test_parsed = gherkin::Feature::parse(gherkin_test_matches, test_env);
+    println!("Test says:\n{:?}", test_parsed);
+    // TODO: Match the gherkin::Feature's Vec<Scenarios> to the regex captures, giving percent match
 }
