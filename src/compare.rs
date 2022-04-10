fdefa2e557786c939f094ba7fe9257ed5bada7b0
Jb DOYON <jb@jiby.tech>
2022-04-11 00:14:21 +0100

    Fix treediff usage to compare Feature struct

--- src/compare.rs
+++ src/compare.rs
@@ -6,6 +6,7 @@
 /// extracted from test code).
 /// TODO Prove this function really works via tests
 /// FIXME: zip iter "reference" exhaustion causes other-exclusive scenarios+steps to wrongly match
+use treediff;
 
 #[allow(clippy::nonminimal_bool)]
 pub fn approximately_eq(reference: &gherkin::Feature, other: &gherkin::Feature) -> bool {
@@ -34,16 +35,36 @@ pub fn approximately_eq(reference: &gherkin::Feature, other: &gherkin::Feature) 
     is_feature_equal && are_scenarios_equal
 }
 
+/// Computes struct-identicality by using treediff lib (struct comparison)
+///
+/// Relies on conversion of gherkin::Feature->serde_json::Value, using Value
+/// trait to run treediff::diff
+#[allow(dead_code)]
+#[allow(clippy::just_underscores_and_digits, clippy::match_like_matches_macro)]
+fn json_diff_equals(reference: &gherkin::Feature, other: &gherkin::Feature) -> bool {
+    let ref_json: serde_json::Value = serde_json::value::to_value(&reference).unwrap();
+    let other_json: serde_json::Value = serde_json::value::to_value(&other).unwrap();
+    let mut record = treediff::tools::Recorder::default();
+    treediff::diff(&ref_json, &other_json, &mut record);
+    if record.calls.len() != 1 {
+        return false;
+    }
+    match record.calls.last().unwrap() {
+        // FIXME skip the Path, Linecol and Span for diff
+        // TODO Use custom treediff::tools::Delegate instead
+        treediff::tools::ChangeType::Unchanged(_, __) => true,
+        _ => false,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::{approximately_eq, json_diff_equals};
+    use std::path::PathBuf;
 
-    use treediff::diff;
-    use treediff::tools::Recorder;
-
-    #[test]
-    fn test_feature_matches_clone() {
-        let scenarios = vec![gherkin::Scenario::builder()
+    /// Sample feature to do some comparisons against
+    fn reference_feature() -> gherkin::Feature {
+        let reference_scenarios: Vec<gherkin::Scenario> = vec![gherkin::Scenario::builder()
             .keyword("Scenario".to_string())
             .name("Dummy scenario to be representative".to_string())
             .steps(vec![gherkin::Step::builder()
@@ -52,23 +73,55 @@ mod tests {
                 .value("something happens".to_string())
                 .build()])
             .build()];
-        let reference = gherkin::Feature::builder()
+        gherkin::Feature::builder()
             .keyword("Feature".to_string())
             .name("Matching a feature with another identical feature".to_string())
             .description(Some("Bleh".to_string()))
-            .scenarios(scenarios)
-            .build();
+            .scenarios(reference_scenarios)
+            .build()
+    }
+
+    #[test]
+    fn test_cloned_feature_nodiff() {
+        let reference = reference_feature();
         let cloned = reference.clone();
         assert!(
             approximately_eq(&reference, &cloned),
             "Identical feature should match clone"
         );
-        // Try out the treediff tool "recorder" to see the recursive comparisons
-        // See also json-patch lib Delegate impl for example of using Delegate
-        // https://github.com/idubrov/json-patch/blob/dfc8943625bb4349a58f432c76e4ef3b6f6814b8/src/diff.rs#L19
-        let mut record = Recorder::default();
-        diff(&reference, &cloned, &mut record);
-        println!("{:?}", record.calls);
-        assert!(false); // intentional fail to see struct content
+
+        assert!(
+            json_diff_equals(&reference, &cloned),
+            "Identical feature should match clone"
+        );
+    }
+
+    #[test]
+    fn test_cloned_feature_noscenarios_nodiff() {
+        let reference = reference_feature();
+
+        let cloned = reference.clone();
+        assert!(
+            json_diff_equals(&reference, &cloned),
+            "Identical feature should match clone"
+        );
+        // FIXME Crash on comparison of features with empty scenarios
+        // assert!(
+        //     approximately_eq(&reference, &cloned),
+        //     "Identical feature should match clone"
+        // );
+    }
+    #[test]
+    fn test_feature_different_path_nodiff() {
+        let mut reference = reference_feature();
+        reference.path = Some(PathBuf::from("/path/to/my/reference.feature"));
+        let mut cloned = reference.clone();
+        cloned.path = Some(PathBuf::from("/path/to/code/test_reference.py"));
+        assert!(
+            approximately_eq(&reference, &cloned),
+            "Similar feature (up to path) should match clone"
+        );
+        // FIXME Known limitation of the current method, not ignoring Path differences
+        // assert!(json_diff_equals(&reference, &cloned), "Similar feature (up to path) should match clone");
     }
 }
