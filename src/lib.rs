fc1ac0bfec83ece7e004546deb3a65499792616e
Jb DOYON <jb@jiby.tech>
2022-04-13 02:23:32 +0100

    Add ideas in comments for matching feature

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,54 @@ impl GherkinProject {
         }
     }
 }
+
+// pub enum GrepkinFeatureType {
+//     Referenced,
+//     Extracted,
+// }
+
+// pub struct GrepkinFeature{
+//     feature: gherkin::Feature,
+//     source: GrepkinFeatureType,
+// }
+
+// Match library idea:
+//
+// - Merge lists of features via GrepkinFeature (convert to Array of Structure)
+// - Allocate HashMap<String, Vec<GrepkinFeature> based on feature.name string
+// - [FUTURE] If features non-allocated, do string similarlity
+// - Check each HashMap key has 2 values: 1 source.Referenced + 1 source.Extracted
+// - If these pairs exist, inspect them pairwise
+// - Pairwise check description
+// - Pairwise descent into scenarios
+// - Scenarios HashMap<String, Vec<GrepkinScenario>> and do same
+// - Same down one more to Steps (group by step types for similarity)
+// - Output list of missing:
+//
+//
+// pub enum GrepkinChangeTag {
+// // Compared to Referenced feature
+//     Added,
+//     Missing,
+//     // [FUTURE] Similarity checking:
+//     Modified,
+//     Reordered,
+// }
+
+// pub enum GrepkinDiffContent {
+//     Feature(gherkin::Feature),
+//     Scenario(gherkin::Scenario),
+//     Step(gherkin::Step),
+// }
+
+// pub struct GrepkinDiff {
+//     tag: GrepkinChangeTag,
+//     reference: GrepkinDiffContent,
+//     extracted: GrepkinDiffContent,
+//     fieldname_diffed: Optional<String>,
+// }
+
+// pub struct GrepkinProjectDiff{
+//     matches: bool,
+//     diff: Vec<GrepkinDiff>
+// }
